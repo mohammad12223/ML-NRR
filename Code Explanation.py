7a92a306d795a5ed019bff2cc3f99375e4ac9c7e
mohammad12223 <58195593+mohammad12223@users.noreply.github.com>
2020-02-25 09:56:42 +0900

    Update Code Explanation.py

--- Code Explanation.py
+++ Code Explanation.py
@@ -1,7 +1,8 @@
 Standardization: 
 
-Preprocessing operation on data before feeding to the neural network is very important. In such a data set that ranges of features are significantly different, it seems not to be reasonable to feed those raw features to network. 
-One of the best method to deal with this problem is using normalization process. In this process the data (each column in data) is centered around with zero mean and unit variance. It can be easily done in Python as follows:
+Preprocessing operation on data before feeding to the neural network is very important. In such a data set that ranges of features are significantly different,
+it seems not to be reasonable to feed those raw features to network. One of the best method to deal with this problem is using normalization process. 
+In this process the data (each column in data) is centered around with zero mean and unit variance. It can be easily done in Python as follows:
 
 mean = data.mean(axis=0)
 data -= mean
@@ -14,8 +15,9 @@ from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X = sc.fit_transform(data)
 
-we found in DNN (deep neural network) that the catalytic activity is mainly governed by seven features: PCA1, electro-negativity of TM (χ), atomic number of TM (Z), atomic radius of TM (R in pm), 
- coordination number of TM (N_C), fraction of B atoms among coordinating atoms in TM-B_xC_y-Gr [f_B=x/(x+y)], and number of nitrogen atoms adsorbed on TM (for side-on and end-on mechanisms-N_M=2/1).
+we found in DNN (deep neural network) that the catalytic activity is mainly governed by seven features: PCA1, electro-negativity of TM (χ), 
+ atomic number of TM (Z), atomic radius of TM (R in pm), coordination number of TM (N_C), fraction of B atoms among coordinating atoms in TM-B_xC_y-Gr [f_B=x/(x+y)], 
+ and number of nitrogen atoms adsorbed on TM (for side-on and end-on mechanisms-N_M=2/1).
 
 
 Adam optimizer was used in DNN that can be summarized as follows:
@@ -30,7 +32,7 @@ m← ß1 · m + (1 - ß1) · g (First update moment)
 v ← ß2 · v + (1 - ß2) · g•g (Second update moment) 
 m_t← m/(1 – ß1 ) (Bias correction in first moment) 
 v_t ← v/(1 – ß2 ) (Bias correction in second moment) 
-θ_t  =θ  - lr_t * m_t / (v(v_t) +ε) (Update parameters) 
+θ_t  ← θ  - lr_t * m_t / (√(v_t) +ε) (Update parameters) 
 
 return θ_t  
 
@@ -51,19 +53,19 @@ from keras import optimizers
 
 
 
-regressor = Sequential()
+classifier = Sequential()
 # hidden layer
-regressor.add(Dense(units =10, activation = ‘tanh',kernel_regularizer=regularizers.l2(0.003), input_dim = X.shape[1]))
+classifier.add(Dense(units =10, activation = 'tanh',kernel_regularizer=regularizers.l2(0.003), input_dim = X.shape[1]))
 # hidden layer
-regressor.add(Dense(units =10, activation = 'tanh', kernel_regularizer=regularizers.l2(0.003) ))
+classifier.add(Dense(units =10, activation = 'tanh', kernel_regularizer=regularizers.l2(0.003) ))
 # output layer
-regressor.add(Dense(units = 1,activation = 'sigmoid'))
+classifier.add(Dense(units = 1,activation = 'sigmoid'))
 #compile DNN
-regressor.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics =['acc'])
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics =['acc'])
 # split into input (X) and output (Y) variables # Fit the model
-history=regressor.fit (X,Y,batch_size = 1, epochs =50, validation_split=0.15)
+history=classifier.fit (X,Y,batch_size = 1, epochs =50, validation_split=0.25)
 # save model and architecture to single file
-regressor.save('nnr_weights.h5')
+classifier.save('nnr_weights.h5')
 history_dict = history.history
 loss_values = history_dict['loss']
 val_loss_values = history_dict['val_loss']
@@ -79,10 +81,7 @@ plt.show()
 
 
 
-
-
-
-
+*****
 
 Some of the hyperparameters used for LightGBM regressor model are as follows:
                     
